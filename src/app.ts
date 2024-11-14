88b00b3a0b5521656f1a6fa6227de6360808a517
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-11-14 18:00:20 +0530

    Add body limit middleware to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Hono, type Context } from "hono";
 import { cors } from "hono/cors";
 import { csrf } from "hono/csrf";
 import { logger } from "hono/logger";
+import { bodyLimit } from "hono/body-limit";
 import { poweredBy } from "hono/powered-by";
 import { prettyJSON } from "hono/pretty-json";
 import { secureHeaders } from "hono/secure-headers";
@@ -18,6 +19,14 @@ app.use(
 );
 app.use(csrf());
 app.use(logger());
+app.use(
+  bodyLimit({
+    maxSize: parseInt(Bun.env.BODY_LIMIT!) * 1024,
+    onError: (c) => {
+      return c.json({ error: "Request payload is too large!" }, 413);
+    },
+  })
+);
 app.use(poweredBy());
 app.use(prettyJSON());
 app.use(secureHeaders());
@@ -27,7 +36,7 @@ app.all("/hello", (c: Context) => {
 });
 
 app.onError((e: Error, c: Context) => {
-  console.log({ e });
+  console.log(`${e.name}: ${e.message}`);
   return c.json({ error: e.message }, 500);
 });
 
