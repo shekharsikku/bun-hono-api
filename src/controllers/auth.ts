cd2995eab2d7803c14f8783cc8af161ecddb92aa
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-01-31 14:21:36 +0530

    Authentication api routes complete

---
+++ src/controllers/auth.ts
@@ -0,0 +1,118 @@
+import type { Context } from "hono";
+import { deleteCookie } from "hono/cookie";
+import { genSalt, hash, compare } from "bcryptjs";
+import { ApiError, ApiResponse } from "../utils";
+import {
+  authorizeCookie,
+  createUserInfo,
+  generateAccess,
+  generateRefresh,
+} from "../helpers";
+import User from "../models/user";
+import env from "../utils/env";
+
+const signUpUser = async (c: Context) => {
+  try {
+    const { email, password } = c.get("validData");
+
+    const existsEmail = await User.exists({ email });
+
+    if (existsEmail) {
+      throw new ApiError(409, "Email already exists!");
+    }
+
+    const hashSalt = await genSalt(12);
+    const hashedPassword = await hash(password, hashSalt);
+
+    const newUser = await User.create({ email, password: hashedPassword });
+
+    return ApiResponse(c, 201, "Signed up successfully!", newUser);
+  } catch (error: any) {
+    return ApiResponse(c, error.code, error.message);
+  }
+};
+
+const signInUser = async (c: Context) => {
+  try {
+    const { email, username, password } = c.get("validData");
+    const conditions = [];
+
+    if (email) {
+      conditions.push({ email });
+    } else if (username) {
+      conditions.push({ username });
+    } else {
+      throw new ApiError(400, "Email or Username required!");
+    }
+
+    const existsUser = await User.findOne({
+      $or: conditions,
+    }).select("+password +authentication");
+
+    if (!existsUser) {
+      throw new ApiError(404, "User not exists!");
+    }
+
+    const isCorrect = await compare(password, existsUser.password!);
+
+    if (!isCorrect) {
+      throw new ApiError(403, "Incorrect password!");
+    }
+
+    const userInfo = createUserInfo(existsUser);
+    const accessToken = await generateAccess(c, userInfo);
+
+    if (!userInfo.setup) {
+      return ApiResponse(c, 202, "Please, complete your profile!", userInfo);
+    }
+
+    const refreshToken = await generateRefresh(c, userInfo._id!);
+    const refreshExpiry = env.REFRESH_EXPIRY;
+
+    existsUser.authentication?.push({
+      token: refreshToken,
+      expiry: new Date(Date.now() + refreshExpiry * 1000),
+    });
+
+    const authorizeUser = await existsUser.save();
+    const authorizeId = authorizeUser.authentication?.filter(
+      (auth) => auth.token === refreshToken
+    )[0]._id!;
+
+    authorizeCookie(c, authorizeId.toString());
+
+    return ApiResponse(c, 200, "Signed in successfully!", userInfo);
+  } catch (error: any) {
+    deleteCookie(c, "access");
+    deleteCookie(c, "refresh");
+    deleteCookie(c, "current");
+    return ApiResponse(c, error.code, error.message);
+  }
+};
+
+const signOutUser = async (c: Context) => {
+  const requestUser = c.req.user!;
+  const refreshToken = deleteCookie(c, "refresh");
+  const authorizeId = deleteCookie(c, "current");
+
+  if (requestUser.setup && refreshToken && authorizeId) {
+    await User.updateOne(
+      { _id: requestUser._id },
+      {
+        $pull: {
+          authentication: { _id: authorizeId, token: refreshToken },
+        },
+      }
+    );
+  }
+
+  deleteCookie(c, "access");
+  return ApiResponse(c, 200, "Signed out successfully!");
+};
+
+const refreshAuth = async (c: Context) => {
+  const refreshData = { user: c.req.user, token: c.req.token };
+  return ApiResponse(c, 200, "Authentication refreshed!");
+};
+
+export { signUpUser, signInUser, signOutUser, refreshAuth };
---
+++ src/routes/auth.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import { authAccess, authRefresh } from "../middlewares";
+import { signInSchema, signUpSchema, validateSchema } from "../utils/schema";
+import {
+  refreshAuth,
+  signInUser,
+  signOutUser,
+  signUpUser,
+} from "../controllers/auth";
+
+const auth = new Hono();
+
+auth.post("/sign-up", validateSchema(signUpSchema), signUpUser);
+auth.post("/sign-in", validateSchema(signInSchema), signInUser);
+auth.delete("/sign-out", authAccess, signOutUser);
+auth.get("/auth-refresh", authRefresh, refreshAuth);
+
+export default auth;
