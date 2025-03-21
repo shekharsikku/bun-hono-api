d5a4560af954cbfb0c009605e3f69a65c3a6ef9c
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-03-21 10:27:34 +0530

    Add message api controllers and routes

---
+++ src/routes/message.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import { authAccess } from "../middlewares";
+import { validateSchema, messageSchema } from "@/utils/schema";
+import {
+  deleteMessages,
+  deleteMessage,
+  editMessage,
+  getMessages,
+  sendMessage,
+} from "@/controllers/message";
+
+const message = new Hono();
+
+message.get("/get/:id", authAccess, getMessages);
+
+message.post(
+  "/send/:id",
+  authAccess,
+  validateSchema(messageSchema),
+  sendMessage
+);
+
+message.patch("/edit/:id", authAccess, editMessage);
+
+message.delete("/delete/:id", authAccess, deleteMessage);
+message.delete("/delete-messages", authAccess, deleteMessages);
+
+export default message;
